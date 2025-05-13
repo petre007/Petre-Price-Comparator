0de82642083acd1188f6453b690b913eb748abec
“petre007” <petredumitru1@gmail.com>
2025-05-14 00:48:34 +0300

    Add ECR terraform module

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,12 @@ module "cognito" {
   source          = "./modules/cognito"
   user_pool_name  = "price-comparator-user-pool"
 }
+
+module "ecr" {
+  source           = "./modules/ecr"
+  repository_name  = "price-comparator-backend"
+  tags = {
+    Environment = "dev"
+    Project     = "price-comparator"
+  }
+}
