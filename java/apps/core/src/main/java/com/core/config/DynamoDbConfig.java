c838443e0f46d9e07e7f57bce108f549991724be
“petre007” <petredumitru1@gmail.com>
2025-05-23 15:55:39 +0300

    Configure DynamoDb client

---
+++ java/apps/core/src/main/java/com/core/config/DynamoDbConfig.java
@@ -0,0 +1,18 @@
+package com.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
+public class DynamoDbConfig {
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.EU_CENTRAL_1) // Change to your region
+                .build();
+    }
+
+}
